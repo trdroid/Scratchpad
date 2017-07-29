290b1c08c058cbc84b8625aecbee9f199e57442f
Jai <gruprog@gmail.com>
2017-07-28 23:09:12 -0700

    HelloWorld in Unity

---
+++ HoloLens/HelloWorld/Assets/CubeColor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeColor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var r = Random.Range(0.0f, 1.0f);
+        var g = Random.Range(0.0f, 1.0f);
+        var b = Random.Range(0.0f, 1.0f);
+
+        var newColor = new Color(r, g, b);
+        this.GetComponent<Renderer>().material.color = newColor;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
